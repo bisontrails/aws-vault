2a6ffef8d109cef66107a42f71a3d8b2f374fa58
Josh Kaufman <70281941+secjosh@users.noreply.github.com>
2021-06-30 21:22:51 -0400

    Update server/ec2proxy_unix.go

--- server/ec2proxy_unix.go
+++ server/ec2proxy_unix.go
@@ -11,7 +11,7 @@ import (
 // StartEc2EndpointProxyServerProcess starts a `aws-vault proxy` process
 func StartEc2EndpointProxyServerProcess() error {
 	log.Println("Starting `aws-vault proxy` as root in the background")
-	/* #nosec */
+	/* #nosec G204 */
 	// Always resolves to aws-vault executable
 	cmd := exec.Command("sudo", "-b", awsVaultExecutable(), "proxy")
 	cmd.Stdin = os.Stdin
